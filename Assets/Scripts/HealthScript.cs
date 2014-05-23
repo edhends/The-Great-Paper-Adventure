ac5468b31892886919c5befef7a85291670405cb
Laura Cossette <llcossette@gmail.com>
2014-05-23 14:17:55 -0700

    adds shot prefab, shot script, and enemy health script

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	// Enemy hp
+	public int hp = 1;
+
+	public bool isEnemy = true;
+
+	// Inflicts damage, destroys enemy if dead
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+		if (hp <= 0) 
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Runs actions when hit, destroys shot
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> ();
+		if (shot != null) 
+		{
+			if (shot.isEnemyShot != isEnemy)
+			{
+				Damage (shot.damage);
+				Destroy (shot.gameObject);
+			}
+		}
+	}
+	
+}
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour 
+{
+	// Set damage
+	public int damage = 1;
+
+	public bool isEnemyShot = false;
+
+
+	void Start () 
+	{
+		Destroy (gameObject, 20);
+	}
+}
