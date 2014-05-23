dcf90d17432e34df1e1ae735ca6f033b6c5dff00
Laura Cossette <llcossette@gmail.com>
2014-05-23 13:09:45 -0700

    adds basic script for player object

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+
+public class PlayerScript : MonoBehaviour 
+	{
+		// Ship speed
+		public Vector2 speed = new Vector2(50,50);
+
+		// Movement is stored
+		private Vector2 movement;
+
+
+	void Update () 
+	{
+	 	// Retrieves axis info
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		// Movement per direction
+		movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+
+	void FixedUpdate()
+	{
+		// Moves the player
+		rigidbody2D.velocity = movement;
+	}
+}
