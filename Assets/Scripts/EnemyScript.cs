bc0ecee6fede296df57d8122395c21dfd6a364f4
Laura Cossette <llcossette@gmail.com>
2014-05-23 19:49:01 -0700

    sky background is looping

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,21 +3,62 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour 
 {
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
 	void Awake() 
 	{
 		weapons = GetComponentsInChildren<WeaponScript> ();
+		moveScript = GetComponent<MoveScript> ();
+	}
+
+	void Start()
+	{
+		hasSpawn = false;
+		collider2D.enabled = false;
+		moveScript.enabled = false;
+		foreach (WeaponScript weapon in weapons) 
+		{
+			weapon.enabled = false;
+		}
 	}
 
 	void Update()
 	{
+		if (hasSpawn == false) 
+		{
+			if (renderer.IsVisibleFrom (Camera.main)) 
+			{
+				Spawn ();
+			}
+		} 
+		else
+		{
+			foreach (WeaponScript weapon in weapons) 
+			{
+				if (weapon != null && weapon.CanAttack) 
+				{
+					weapon.Attack (true);
+				}
+			}
+
+			if (renderer.IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+	}
+
+	private void Spawn()
+	{
+		hasSpawn = true;
+		collider2D.enabled = true;
+		moveScript.enabled = true;
 		foreach (WeaponScript weapon in weapons) 
 		{
-			if (weapon != null && weapon.CanAttack) 
-			{
-				weapon.Attack (true);
-			}
+			weapon.enabled = true;
 		}
 	}
 }
--- Assets/Scripts/ScrollingScript.cs
+++ Assets/Scripts/ScrollingScript.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using System.Collections;
 
 public class ScrollingScript : MonoBehaviour 
@@ -6,6 +8,28 @@ public class ScrollingScript : MonoBehaviour
 	public Vector2 speed = new Vector2(2, 2);
 	public Vector2 direction = new Vector2(-1,0);
 	public bool isLinkedToCamera = false;
+	public bool isLooping = false;
+	private List<Transform> backgroundPart;
+
+	void Start()
+	{
+		if (isLooping) 
+		{
+			backgroundPart = new List<Transform>();
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				Transform child = transform.GetChild(i);
+				if (child.renderer != null)
+				{
+					backgroundPart.Add(child);
+				}
+			}
+
+			backgroundPart = backgroundPart.OrderBy(
+				t => t.position.x
+				).ToList();
+		}
+	}
 
 
 	void Update () 
@@ -19,6 +43,26 @@ public class ScrollingScript : MonoBehaviour
 		if (isLinkedToCamera) 
 		{
 			Camera.main.transform.Translate(movement);
+		}
+		if (isLooping) 
+		{
+			Transform firstChild = backgroundPart.FirstOrDefault();
+			if (firstChild != null)
+			{
+				if (firstChild.position.x < Camera.main.transform.position.x)
+				{
+					if (firstChild.renderer.IsVisibleFrom(Camera.main) == false)
+					{
+						Transform lastChild = backgroundPart.LastOrDefault();
+						Vector3 lastPosition = lastChild.transform.position;
+						Vector3 lastSize = (lastChild.renderer.bounds.max - lastChild.renderer.bounds.min);
+						firstChild.position = new Vector3(lastPosition.x + lastSize.x, 
+							                                       firstChild.position.y, firstChild.position.z);
+						backgroundPart.Remove(firstChild);
+						backgroundPart.Add(firstChild);
+					}
+				}
+			}
 		}
 	}
 }
